74d03d38482f85f1f22cc2a289a35ccd6452c1ba
nachiketWattamwar <nachiket.wattamwar@sjsu.edu>
2020-02-24 20:31:28 -0800

    added css to expenses page

--- moneymatters/src/actions/recentexpense.js
+++ moneymatters/src/actions/recentexpense.js
@@ -4,12 +4,8 @@ import { GET_RECENT_EXPENSE, GET_RECENT_EXPENSES_FIVE } from "./types";
 
 export const getRecentExpense = () => async dispatch => {
   try {
-    //console.log("===============before axios call fist");
     const res = await axios.get(`http://localhost:3001/api`);
-    // console.log(
-    //   "==================================after axios call=======",
-    //   res
-    // );
+
     dispatch({
       type: GET_RECENT_EXPENSE,
       payload: res.data
@@ -22,12 +18,8 @@ export const getRecentExpense = () => async dispatch => {
 export const getRecentExpensesFive = () => async dispatch => {
   console.log("Inside recent 5");
   try {
-    //console.log("===============before axios call");
     const res = await axios.get(`http://localhost:3001/recentFiveExpenses`);
-    // console.log(
-    //   "==================================after axios call=======",
-    //   res
-    // );
+
     dispatch({
       type: GET_RECENT_EXPENSES_FIVE,
       payload: res.data
