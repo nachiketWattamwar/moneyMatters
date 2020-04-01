94c7ea9b45c6ec54e5069d03091a37df12ac158b
Nikhil Limaye <nikhillimaye10@gmail.com>
2020-04-01 15:23:55 -0700

    Expense Routes - Login bug fixes

---
+++ moneymatters/backend/middleware/uservalidity.js
@@ -0,0 +1,22 @@
+const User = require('../models/user')
+
+const uservalidity = async (req , res , next) => {
+      
+    try {
+        const userObj = await User.findOne({email : req.body.email})
+
+        if(userObj == null) {
+            console.log("User not found - ",req.body.email)
+            return res.status(400).send("No such user found");
+        }
+    }
+    catch(err) {
+        return res.status(400).send(err);
+    }
+
+    console.log("User validation for "+req.body.email+" successful")
+    next();
+}
+
+
+module.exports = uservalidity
