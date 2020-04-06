19ef7c63469b8db6cf124e2d6b87533f7b8e2f50
Jai <jai.chhatre@gmail.com>
2020-04-06 14:07:53 -0700

    ablt to add, update and delete data fom expenses and goals table

--- moneymatters/src/App.js
+++ moneymatters/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./Dashboard";
 import Test from "./components/Test";
-import Goals from "./components/Goals";
+//import Goals from "./components/Goals";
+import Goals from "./components/FinancialGoals";
+
 import Signup from "./components/Signup";
 import DetailFinances from "./components/DetailFinances";
 import { Provider } from "react-redux";
@@ -10,21 +12,21 @@ import store from "./store";
 import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./components/Login";
 function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <Route path='/' exact component={Login} />
-          <Route path='/dashboard' exact component={Dashboard} />
-          <Route path='/expenses' exact component={Test} />
-          <Route path='/goals' exact component={Goals} />
-          <Route path='/signup' exact component={Signup} />
-          <Route path='/login' exact component={Login} />
-          <Route path='/detailFinances' exact component={DetailFinances} />
-        </div>
-      </BrowserRouter>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<BrowserRouter>
+				<div>
+					<Route path='/' exact component={Login} />
+					<Route path='/dashboard' exact component={Dashboard} />
+					<Route path='/expenses' exact component={Test} />
+					<Route path='/goals' exact component={Goals} />
+					<Route path='/signup' exact component={Signup} />
+					<Route path='/login' exact component={Login} />
+					<Route path='/detailFinances' exact component={DetailFinances} />
+				</div>
+			</BrowserRouter>
+		</Provider>
+	);
 }
 
 export default App;
